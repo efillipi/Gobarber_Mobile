4abca38c2c3781a65c43ec5fcd880a162ea04fa9
Edney Fillipi <e.fillipi@gmail.com>
2021-08-16 20:08:22 -0300

    :sparkles: Paginas / Rotas

--- src/routes/StackParamList.ts
+++ src/routes/StackParamList.ts
@@ -3,6 +3,10 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 type StackParamList = {
   SignUp: undefined;
   SignIn: undefined;
+  Dashboard: undefined;
+  Profile: undefined;
+  AppointmentDatePicker: undefined;
+  AppointmentCreated: undefined;
 };
 
 export type ProfileScreenNavigationProp =
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,17 +2,26 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+import AppointmentDatePicker from '../pages/AppointmentDatePicker';
+import AppointmentCreated from '../pages/AppointmentCreated';
 
 const App = createNativeStackNavigator();
 
 const AppRoutes: React.FC = () => (
   <App.Navigator
     screenOptions={{
-      // headerShown: false,
+      headerShown: false,
       contentStyle: { backgroundColor: '#312e38' },
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="Profile" component={Profile} />
+    <App.Screen
+      name="AppointmentDatePicker"
+      component={AppointmentDatePicker}
+    />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
   </App.Navigator>
 );
 
