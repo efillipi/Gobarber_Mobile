67e717242063ad5230da6fa7514a660ee1aab70a
Edney Fillipi <e.fillipi@gmail.com>
2021-09-02 18:03:44 -0300

    :sparkles: Retirando dados não usados

--- src/routes/app.drawer.routes.tsx
+++ src/routes/app.drawer.routes.tsx
@@ -6,9 +6,6 @@ import DrawerNavigation from '../components/DrawerNavigation';
 import AppProvider from './appProvider.routes';
 import AppUser from './appUser.routes';
 import AppointmentsClient from '../pages/AppointmentsClient';
-import AppointmentsProvider from '../pages/AppointmentsProvider';
-import Portiforlio from '../pages/Portiforlio';
-import ServicesProvider from '../pages/ServicesProvider';
 
 import Profile from '../pages/Profile';
 
@@ -16,7 +13,7 @@ const Drawer = createDrawerNavigator();
 
 const AppRoutes: React.FC = () => {
   const { user } = useAuth();
-  return user.role === 'Provider' ? <MyDrawerProvider /> : <MyDrawerClient />;
+  return user.role === 'Provider' ? <AppProvider /> : <MyDrawerClient />;
 };
 
 const MyDrawerClient: React.FC = () => {
@@ -56,62 +53,6 @@ const MyDrawerClient: React.FC = () => {
         }}
         name="Agendamentos"
         component={AppointmentsClient}
-      />
-    </Drawer.Navigator>
-  );
-};
-
-const MyDrawerProvider: React.FC = () => {
-  return (
-    <Drawer.Navigator
-      drawerContent={(props) => <DrawerNavigation props={{ ...props }} />}
-      screenOptions={{
-        headerShown: false,
-        sceneContainerStyle: { backgroundColor: '#312e38' },
-        drawerStyle: { backgroundColor: '#312e38', width: '65%' },
-        drawerActiveBackgroundColor: '#ff9000',
-        drawerInactiveBackgroundColor: '#3e3b47',
-        drawerActiveTintColor: '#312e38',
-        drawerInactiveTintColor: '#fff',
-        drawerLabelStyle: {
-          fontSize: 18,
-        },
-      }}
-    >
-      <Drawer.Screen
-        options={{
-          drawerIcon: () => <Icon name="home" size={24} color="#312e38" />,
-        }}
-        name="Home"
-        component={AppProvider}
-      />
-      <Drawer.Screen
-        options={{
-          drawerIcon: () => <Icon name="smile" size={24} color="#312e38" />,
-        }}
-        name="Perfil"
-        component={Profile}
-      />
-      <Drawer.Screen
-        options={{
-          drawerIcon: () => <Icon name="calendar" size={24} color="#312e38" />,
-        }}
-        name="Agendamentos"
-        component={AppointmentsProvider}
-      />
-      <Drawer.Screen
-        options={{
-          drawerIcon: () => <Icon name="image" size={24} color="#312e38" />,
-        }}
-        name="Portiforlio"
-        component={Portiforlio}
-      />
-      <Drawer.Screen
-        options={{
-          drawerIcon: () => <Icon name="briefcase" size={24} color="#312e38" />,
-        }}
-        name="Serviços"
-        component={ServicesProvider}
       />
     </Drawer.Navigator>
   );
